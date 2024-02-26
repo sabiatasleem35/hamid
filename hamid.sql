f408ee9e4895f373188c4dcab6b389f5ac00ffa9
sabiatasleem35 <109856611+sabiatasleem35@users.noreply.github.com>
2024-02-26 20:53:14 +0530

    Add files via upload

---
+++ hamid.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2024 at 03:39 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hamid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_actions`
+--
+-- Error reading structure for table hamid.wp_actionscheduler_actions: #1932 - Table 'hamid.wp_actionscheduler_actions' doesn't exist in engine
+-- Error reading data for table hamid.wp_actionscheduler_actions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_actionscheduler_actions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_claims`
+--
+-- Error reading structure for table hamid.wp_actionscheduler_claims: #1932 - Table 'hamid.wp_actionscheduler_claims' doesn't exist in engine
+-- Error reading data for table hamid.wp_actionscheduler_claims: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_actionscheduler_claims`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_groups`
+--
+-- Error reading structure for table hamid.wp_actionscheduler_groups: #1932 - Table 'hamid.wp_actionscheduler_groups' doesn't exist in engine
+-- Error reading data for table hamid.wp_actionscheduler_groups: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_actionscheduler_groups`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_actionscheduler_logs`
+--
+-- Error reading structure for table hamid.wp_actionscheduler_logs: #1932 - Table 'hamid.wp_actionscheduler_logs' doesn't exist in engine
+-- Error reading data for table hamid.wp_actionscheduler_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_actionscheduler_logs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_addonlibrary_addons`
+--
+-- Error reading structure for table hamid.wp_addonlibrary_addons: #1932 - Table 'hamid.wp_addonlibrary_addons' doesn't exist in engine
+-- Error reading data for table hamid.wp_addonlibrary_addons: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_addonlibrary_addons`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_addonlibrary_categories`
+--
+-- Error reading structure for table hamid.wp_addonlibrary_categories: #1932 - Table 'hamid.wp_addonlibrary_categories' doesn't exist in engine
+-- Error reading data for table hamid.wp_addonlibrary_categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_addonlibrary_categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_audit_log`
+--
+-- Error reading structure for table hamid.wp_aiowps_audit_log: #1932 - Table 'hamid.wp_aiowps_audit_log' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_audit_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_audit_log`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_debug_log`
+--
+-- Error reading structure for table hamid.wp_aiowps_debug_log: #1932 - Table 'hamid.wp_aiowps_debug_log' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_debug_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_debug_log`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_events`
+--
+-- Error reading structure for table hamid.wp_aiowps_events: #1932 - Table 'hamid.wp_aiowps_events' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_events: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_events`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_global_meta`
+--
+-- Error reading structure for table hamid.wp_aiowps_global_meta: #1932 - Table 'hamid.wp_aiowps_global_meta' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_global_meta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_global_meta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_logged_in_users`
+--
+-- Error reading structure for table hamid.wp_aiowps_logged_in_users: #1932 - Table 'hamid.wp_aiowps_logged_in_users' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_logged_in_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_logged_in_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_login_lockdown`
+--
+-- Error reading structure for table hamid.wp_aiowps_login_lockdown: #1932 - Table 'hamid.wp_aiowps_login_lockdown' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_login_lockdown: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_login_lockdown`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_message_store`
+--
+-- Error reading structure for table hamid.wp_aiowps_message_store: #1932 - Table 'hamid.wp_aiowps_message_store' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_message_store: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_message_store`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_aiowps_permanent_block`
+--
+-- Error reading structure for table hamid.wp_aiowps_permanent_block: #1932 - Table 'hamid.wp_aiowps_permanent_block' doesn't exist in engine
+-- Error reading data for table hamid.wp_aiowps_permanent_block: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_aiowps_permanent_block`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_commentmeta`
+--
+-- Error reading structure for table hamid.wp_commentmeta: #1932 - Table 'hamid.wp_commentmeta' doesn't exist in engine
+-- Error reading data for table hamid.wp_commentmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_commentmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_comments`
+--
+-- Error reading structure for table hamid.wp_comments: #1932 - Table 'hamid.wp_comments' doesn't exist in engine
+-- Error reading data for table hamid.wp_comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_comments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_e_events`
+--
+-- Error reading structure for table hamid.wp_e_events: #1932 - Table 'hamid.wp_e_events' doesn't exist in engine
+-- Error reading data for table hamid.wp_e_events: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_e_events`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_links`
+--
+-- Error reading structure for table hamid.wp_links: #1932 - Table 'hamid.wp_links' doesn't exist in engine
+-- Error reading data for table hamid.wp_links: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_links`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_options`
+--
+-- Error reading structure for table hamid.wp_options: #1932 - Table 'hamid.wp_options' doesn't exist in engine
+-- Error reading data for table hamid.wp_options: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_options`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_postmeta`
+--
+-- Error reading structure for table hamid.wp_postmeta: #1932 - Table 'hamid.wp_postmeta' doesn't exist in engine
+-- Error reading data for table hamid.wp_postmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_postmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
+-- Error reading structure for table hamid.wp_posts: #1932 - Table 'hamid.wp_posts' doesn't exist in engine
+-- Error reading data for table hamid.wp_posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_termmeta`
+--
+-- Error reading structure for table hamid.wp_termmeta: #1932 - Table 'hamid.wp_termmeta' doesn't exist in engine
+-- Error reading data for table hamid.wp_termmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_termmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_terms`
+--
+-- Error reading structure for table hamid.wp_terms: #1932 - Table 'hamid.wp_terms' doesn't exist in engine
+-- Error reading data for table hamid.wp_terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_relationships`
+--
+-- Error reading structure for table hamid.wp_term_relationships: #1932 - Table 'hamid.wp_term_relationships' doesn't exist in engine
+-- Error reading data for table hamid.wp_term_relationships: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_term_relationships`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_taxonomy`
+--
+-- Error reading structure for table hamid.wp_term_taxonomy: #1932 - Table 'hamid.wp_term_taxonomy' doesn't exist in engine
+-- Error reading data for table hamid.wp_term_taxonomy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_term_taxonomy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_usermeta`
+--
+-- Error reading structure for table hamid.wp_usermeta: #1932 - Table 'hamid.wp_usermeta' doesn't exist in engine
+-- Error reading data for table hamid.wp_usermeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_usermeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_users`
+--
+-- Error reading structure for table hamid.wp_users: #1932 - Table 'hamid.wp_users' doesn't exist in engine
+-- Error reading data for table hamid.wp_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_payments`
+--
+-- Error reading structure for table hamid.wp_wpforms_payments: #1932 - Table 'hamid.wp_wpforms_payments' doesn't exist in engine
+-- Error reading data for table hamid.wp_wpforms_payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_wpforms_payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_payment_meta`
+--
+-- Error reading structure for table hamid.wp_wpforms_payment_meta: #1932 - Table 'hamid.wp_wpforms_payment_meta' doesn't exist in engine
+-- Error reading data for table hamid.wp_wpforms_payment_meta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_wpforms_payment_meta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_wpforms_tasks_meta`
+--
+-- Error reading structure for table hamid.wp_wpforms_tasks_meta: #1932 - Table 'hamid.wp_wpforms_tasks_meta' doesn't exist in engine
+-- Error reading data for table hamid.wp_wpforms_tasks_meta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hamid`.`wp_wpforms_tasks_meta`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
